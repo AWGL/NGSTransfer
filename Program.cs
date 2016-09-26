fbb3cde060fea370bfc19721e85db87c4209e14b
Matthew Lyon <matt.lyon@wales.nhs.uk>
2016-09-26 15:22:41 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace NGSTransferConsole
 {
     class Program
     {
-        public const string programVersion = "1.0.2";
+        public const string programVersion = "1.0.3";
         private static Config config = new Config();
 
         static void Main(string[] args)
